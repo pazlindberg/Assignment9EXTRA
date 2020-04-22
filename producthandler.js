78db259af9a890afcf8b9ed00a53353572d8a3bd
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-04-22 09:31:12 +0200

    d2init

--- producthandler.js
+++ producthandler.js
@@ -27,7 +27,9 @@ function BuyProduct(prod)
 	//var t=products[x-1];
 
 	let t='id="product'+prod+'"';
-	let o='onclick="BuyProduct('+ prod +')" '; 
+	let o='onclick="BuyProduct('+ prod +')" '; //todo swap to remove function but decide how to handle array first (id 
+												//unable to sync with length so 1) reset values, 2) or just search the smallest 
+												//one and use that? 2 seems most effective
 	let striking='style="text-decoration:line-through;" ';
 	let s='<a href="#" ' +  t + o + striking +'>' + temp +"!!" + '</a>';
 	//let s=temp;//'<h2>hej</h2>';
@@ -43,7 +45,7 @@ function AddProduct()
 	let inputVal=document.getElementById("product").value;
 	if(inputVal.length>0)
 	{
-		let t='id="product'+(products.length+1)+'"';
+		let t='id="product'+(products.length+1)+'"'; //todo: look for first empty
 		let o='onclick="BuyProduct('+ (products.length+1) +')"'; 
 		let s='<a href="#" ' +  t + o + '>' + inputVal + '</a>';
 		products.push(s);
